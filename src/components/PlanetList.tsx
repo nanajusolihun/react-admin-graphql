6728086c28455d1f713ea4eccc71c19d27c12606
Nana Jusolihun <nanajusolihun@gmail.com>
2024-01-30 16:20:22 +0700

    feat: add sort, filter, pagination

--- src/components/PlanetList.tsx
+++ src/components/PlanetList.tsx
@@ -1,10 +1,21 @@
 // PlanetList.tsx
 import * as React from "react";
-import { List, Datagrid, FunctionField, TextField, SearchInput, SelectInput } from "react-admin";
+import { 
+  List, 
+  Datagrid, 
+  FunctionField, 
+  TextField, 
+  SearchInput, 
+  SelectInput, 
+  DateField, 
+  NumberField,
+  Pagination,
+} from "react-admin";
 
 interface Planet {
   id: string;
   name: string;
+  created: number;
   filmConnection: {
     films: {
       id: string;
@@ -13,18 +24,30 @@ interface Planet {
   };
 }
 
-const postFilters = [<SearchInput source="q" alwaysOn resettable={false} />, <SelectInput source="name" />];
+const planetFilters = [
+  <SearchInput source="q" alwaysOn resettable={false} />, 
+  <TextField source="name" label="Name" sort={{ field: "name", order: "ASC" }} />, 
+  <SelectInput source="name" />
+];
+
+const PlanetPagination = () => <Pagination rowsPerPageOptions={[5, 10, 15, 25]} />;
 
 const PlanetList: React.FC = (props) => (
-  <List {...props} filters={postFilters}>
+  <List {...props} filters={planetFilters} sort={{ field: "name", order: "ASC" }} pagination={<PlanetPagination />}>
     <Datagrid rowClick="edit">
       <TextField source="name" name="Planets" />
+      <DateField source="created" name="Planets" />
+      <TextField source="climates" />
+      <NumberField source="population" />
+      <TextField source="terrains" />
       <FunctionField
         label="Nama Planet"
         render={(record: Planet) => (
-          <>
-            <h1>{record.filmConnection.films[0].title}</h1>
-          </>
+          <ul>
+            {record.filmConnection.films.map((film) => (
+              <li key={film.id}>{film.title}</li>
+            ))}
+          </ul>
         )}
       />
     </Datagrid>
