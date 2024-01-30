7f80611f73d01aa964b56af82d88f8f8f6403935
Nana Jusolihun <nanajusolihun@gmail.com>
2024-01-30 16:12:27 +0700

    feat: add query

--- src/source/graphql/queries.ts
+++ src/source/graphql/queries.ts
@@ -3,44 +3,89 @@ import { GetListResult } from "react-admin";
 export const queriesPlanet = () => {
   return {
     query: gql`
-      query Planets {
-        allPlanets {
+      query Planets($allPlanetsAfter2: String, $allPlanetsBefore2: String, $allPlanetsFirst2: Int, $allPlanetsLast2: Int) {
+        allPlanets(after: $allPlanetsAfter2, before: $allPlanetsBefore2, first: $allPlanetsFirst2, last: $allPlanetsLast2) {
+          totalCount
           planets {
             id
             name
+            created
+            population
             filmConnection {
+              totalCount
+              pageInfo {
+                startCursor
+                hasPreviousPage
+                hasNextPage
+                endCursor
+              }
               films {
                 id
                 title
               }
             }
+            climates
+            terrains
           }
-          totalCount
+          pageInfo {
+            startCursor
+            hasPreviousPage
+            hasNextPage
+            endCursor
+          }
+          edges {
+            node {
+              id
+              name
+            }
+            cursor
+          }
         }
       }
     `,
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     parseResponse: async (response: Record<string, any>) => {
-      console.log("parserespon planet", response.data);
-
       const datas = await response.data.allPlanets;
+      console.log("dataaa", datas);
       const result: GetListResult = {
         data: datas.planets,
+        pageInfo: datas.pageInfo,
         total: datas.totalCount,
+        edges: datas.edges,
       };
+
       return result;
     },
   };
 };
+
 // Corrected StarshipQueries
 export const queriesStarship = () => {
   return {
     query: gql`
-      query Starship {
-        allStarships {
+      query Starship($after: String, $before: String, $first: Int, $last: Int) {
+        allStarships(after: $after, before: $before, first: $first, last: $last) {
+          edges {
+            cursor
+            node {
+              id
+              name
+            }
+          }
+          pageInfo {
+            endCursor
+            hasNextPage
+            hasPreviousPage
+            startCursor
+          }
           starships {
             id
             name
+            model
+            manufacturers
+            passengers
+            edited
             filmConnection {
               films {
                 id
@@ -58,7 +103,9 @@ export const queriesStarship = () => {
       const datas = await response.data.allStarships;
       const result: GetListResult = {
         data: datas.starships,
+        pageInfo: datas.pageInfo,
         total: datas.totalCount,
+        edges: datas.edges,
       };
       return result;
     },
